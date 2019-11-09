b3d950ac73ffebf0246d56331bc1a0950b5ae842
Joshua Castor <joshua.castor@joshuacastor.me>
2019-11-08 20:27:58 -0600

    Start from a clean repo

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export enum BF_SERVERS {GLOBAL = 'gl', EUROPE = 'eu', JAPAN = 'jp'}
-
-export enum TARGET_AREA_MAPPING {aoe = 'AOE', single = 'ST', random = 'RT'}
--- typedoc.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  includeDeclarations: true,
-  excludeExternals: true,
-  excludePrivate: true,
-  target: "ES5",
-  tsconfig: 'tsconfig.json',
-  readme: 'README.md',
-  exclude: 'src/index.ts',
-};
