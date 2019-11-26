3816fb721745a270fe819c1e20205666f4474848
Joshua Castor <joshua.castor@joshuacastor.me>
2019-11-26 06:39:27 -0600

    added gold flow unit tests for combineEffectsAndDamageFrames

--- test/helpers/utils.js
+++ test/helpers/utils.js
@@ -13,18 +13,18 @@ function getStringValueForLog (input) {
  * @param {{ [key: string]: (index: number) => any }} schema an object that tells what top-level keys the resulting object should contain and the default getters for each property
  * @param {object} options options used to customize factory behavior
  * @param {boolean?} options.deleteUndefined whether to delete `undefined` values from the resulting object
- * @returns {(numEntries: number, valueGetter?: (propName: string, index: number) => any) => any[]} resulting factory function
+ * @returns {(numEntries: number, valueGetter?: (propName: string, index: number, defaultValue: any) => any) => any[]} resulting factory function
  */
 function createObjectListFactoryFromSchema (schema, { deleteUndefined = true } = {}) {
 	const keys = Object.keys(schema);
 
 	/**
 	 * @param {number} numEntries number of entries to generate
-	 * @param {(propName: string, index: number) => any} valueGetter function used to get the value for a given property name and index
+	 * @param {(propName: string, index: number, defaultValue: any) => any} valueGetter function used to get the value for a given property name and index
 	 */
 	return function (numEntries, valueGetter) {
 		const valueGetterIsFunction = typeof valueGetter === 'function';
-		const getValueOrDefault = (propName, index, defaultValue) => valueGetterIsFunction ? valueGetter(propName, index) : defaultValue;
+		const getValueOrDefault = (propName, index, defaultValue) => valueGetterIsFunction ? valueGetter(propName, index, defaultValue) : defaultValue;
 		const result = Array.from(
 			{ length: numEntries },
 			(_, index) => {
--- test/src/buffs.spec.js
+++ test/src/buffs.spec.js
@@ -1,4 +1,5 @@
 const buffUtilities = require('../../src/buffs');
+const datamineTypes = require('../../src/datamine-types');
 const testConstants = require('../helpers/constants');
 const { getStringValueForLog, createObjectListFactoryFromSchema } = require('../helpers/utils');
 
@@ -45,7 +46,7 @@ describe('buff utilties', () => {
 		});
 	});
 
-	describe('combineEffectsAndDamageFrames', () => {
+	describe('combineEffectsAndDamageFrames method', () => {
 		const ARBITRARY_TARGET_AREA = 'arbitrary target area';
 		const ARBITRARY_TARGET_TYPE = 'arbitrary target type';
 		const assertIsArray = (result) => {
@@ -119,6 +120,109 @@ describe('buff utilties', () => {
 			const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
 			assertIsArray(result);
 			expect(result.length).toBe(0);
+		});
+
+		describe('when both effects array and damage frame arrays are the same length and correct shape', () => {
+			const expectedResultKeys = ['delay', 'effect', 'frames', 'id', 'targetArea', 'targetType'].sort();
+			/**
+			 * @param {import('../../src/buffs').IProcEffectFrameComposite[]} result
+			 * @param {import('../../src/datamine-types').ProcEffect[]} inputEffects
+			 * @param {import('../../src/datamine-types').IDamageFramesEntry[]} damageFrames
+			 */
+			const assertResult = (result = [], inputEffects = [], inputDamageFrames = []) => {
+				expect(result.length).toBe(inputEffects.length, `Length of result array [${result.length}] did not match length of input effects array [${inputEffects.length}]`);
+				expect(result.length).toBe(inputDamageFrames.length, `Length of result array [${result.length}] did not match length of input damage frames array [${inputDamageFrames.length}]`);
+				result.forEach((entry, index) => {
+					const correspondingEffect = inputEffects[index];
+					const correspondingDamageFrame = inputDamageFrames[index];
+
+					expect(Object.keys(entry).sort()).toEqual(expectedResultKeys);
+					expect(entry.delay).toBe(correspondingEffect['effect delay time(ms)/frame']);
+					expect(entry.effect).toBe(correspondingEffect);
+					expect(entry.frames).toBe(correspondingDamageFrame);
+
+					if (correspondingEffect['proc id']) {
+						expect(entry.id).toBe(correspondingEffect['proc id']);
+					} else {
+						expect(entry.id).toBe(correspondingEffect['unknown proc id']);
+					}
+
+					if (correspondingEffect['random attack']) {
+						expect(entry.targetArea).toBe(datamineTypes.TargetArea.Random);
+					} else {
+						expect(entry.targetArea).toBe(correspondingEffect['target area']);
+					}
+
+					expect(entry.targetType).toBe(correspondingEffect['target type']);
+				});
+			};
+
+			it('returns the expected composite values when using proc id', () => {
+				const effectsList = generateEffectsList(10);
+				const damageFramesList = generateDamageFramesList(10);
+				const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
+				assertResult(result, effectsList, damageFramesList);
+			});
+
+			it('returns the expected composite values when using unknown proc id', () => {
+				const effectsList = generateEffectsList(10, (propName, index, defaultValue) => {
+					switch (propName) {
+					case 'unknown proc id':
+						return `id-${index}`;
+					case 'proc id':
+						return undefined;
+					default:
+						return defaultValue;
+					}
+				});
+				const damageFramesList = generateDamageFramesList(10);
+				const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
+				assertResult(result, effectsList, damageFramesList);
+			});
+
+			it('returns the expected composite values when using a mix of unknown proc id and proc id', () => {
+				const effectsList = generateEffectsList(10, (propName, index, defaultValue) => {
+					switch (propName) {
+					case 'unknown proc id':
+						return (index % 2 === 0) ? `id-${index}` : undefined;
+					case 'proc id':
+						return (index % 2 === 1) ? `id-${index}` : undefined;
+					default:
+						return defaultValue;
+					}
+				});
+				const damageFramesList = generateDamageFramesList(10);
+				const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
+				assertResult(result, effectsList, damageFramesList);
+			});
+
+			it('returns the expected composite values when using effects with a target area of random attack', () => {
+				const effectsList = generateEffectsList(10, (propName, index, defaultValue) => {
+					switch (propName) {
+					case 'random attack':
+						return true;
+					default:
+						return defaultValue;
+					}
+				});
+				const damageFramesList = generateDamageFramesList(10);
+				const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
+				assertResult(result, effectsList, damageFramesList);
+			});
+
+			it('returns the expected composite values when using effects with a mix of target areas including random attack', () => {
+				const effectsList = generateEffectsList(10, (propName, index, defaultValue) => {
+					switch (propName) {
+					case 'random attack':
+						return index % 2 === 0;
+					default:
+						return defaultValue;
+					}
+				});
+				const damageFramesList = generateDamageFramesList(10);
+				const result = buffUtilities.combineEffectsAndDamageFrames(effectsList, damageFramesList);
+				assertResult(result, effectsList, damageFramesList);
+			});
 		});
 	});
 });
