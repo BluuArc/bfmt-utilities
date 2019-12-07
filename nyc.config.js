a56110cab39aafa0c14fb37bf621d8b19df68015
Joshua Castor <joshua.castor@joshuacastor.me>
2019-12-06 18:43:43 -0600

    Added nyc.config.js to watcher for rerunning tests, added coverage exclude for transpiled index.js file

--- nyc.config.js
+++ nyc.config.js
@@ -1,3 +1,8 @@
+const defaultExcludes = require('@istanbuljs/schema').nyc.properties.exclude.default;
+
 module.exports = {
 	'check-coverage': true,
+	exclude: defaultExcludes.concat([
+		'src/index.js', // excluded to avoid testing transpiled importStar code
+	]),
 };
--- tasks/index.js
+++ tasks/index.js
@@ -15,7 +15,7 @@ function runDev () {
 	const onChange = series(cleanupTasks.cleanupCoverageFiles, testTasks.runCoverage, fullBuild, notifyReadiness);
 	watch(['../src/**/*.ts', '../.eslintrc.ts.js', '!../src/**/*.d.ts'], onChange);
 	watch(['../typedoc.config.js', '../README.md'], series(buildTasks.buildDocs, notifyReadiness));
-	watch(['../test/**/*.js', '../test/jasmine.json'], series(testTasks.lintTests, cleanupTasks.cleanupCoverageFiles, testTasks.runCoverage, notifyReadiness));
+	watch(['../test/**/*.js', '../test/jasmine.json', '../nyc.config.js'], series(testTasks.lintTests, cleanupTasks.cleanupCoverageFiles, testTasks.runCoverage, notifyReadiness));
 	watch(['./**/*.js'], series(testTasks.lintTasks, notifyReadiness));
 	return onChange();
 }
