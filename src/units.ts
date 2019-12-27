8c6fb9a04501dbfe7448ca3288afd8e7c4a83f8f
Joshua Castor <joshua.castor@joshuacastor.me>
2019-12-27 11:14:42 -0600

    Add missing JSDoc comments for units

--- src/units.ts
+++ src/units.ts
@@ -9,6 +9,7 @@ export interface IUnitImageFileNames {
  * @description Generate the file names for each of the image type for a given unit ID
  * @param id the unit ID to use to generate the file names
  * @param suffix optional parameter that's useful for things like alternate art
+ * @returns set of file names for each image type (spritesheet, battle avatar, guide avatar, full illustration)
  */
 export function getUnitImageFileNames (id: string, suffix = ''): IUnitImageFileNames {
 	const fileNameSuffix = `${id || ''}${suffix || ''}.png`;
@@ -24,6 +25,7 @@ export function getUnitImageFileNames (id: string, suffix = ''): IUnitImageFileN
  * @description Generate a URL to display the image with the given unit filename
  * @param baseContentUrl Base URL of the server
  * @param fileName name of the file that represents an image for a given unit
+ * @returns generated URL based on the given content URL and file name
  */
 export function getUnitImageUrl (baseContentUrl: string, fileName: string): string {
 	return `${baseContentUrl || ''}/unit/img/${fileName || ''}`;
