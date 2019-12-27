97a839986375626fc2f5b536daf605dca5b39a6b
Joshua Castor <joshua.castor@joshuacastor.me>
2019-12-27 11:07:21 -0600

    Add missing JSDoc comments for leader skills

--- src/leader-skills.ts
+++ src/leader-skills.ts
@@ -3,6 +3,7 @@ import { ILeaderSkill, PassiveEffect } from './datamine-types';
 /**
  * @description Get the effects of a given leader skill
  * @param skill leader skill to get the effects of
+ * @returns the effects of the given leader skill if they exist, an empty array otherwise
  */
 export function getEffectsForLeaderSkill (skill: ILeaderSkill): PassiveEffect[] {
 	return skill && Array.isArray(skill.effects) ? skill.effects : [];
