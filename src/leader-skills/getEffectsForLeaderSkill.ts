0e5841e8a701f5b0c5b0bcb86dc41a780557c001
BluuArc <joshua.castor@joshuacastor.me>
2020-06-21 21:42:01 -0500

    Split leader-skills mono-module into separate files

--- src/leader-skills/getEffectsForLeaderSkill.ts
+++ src/leader-skills/getEffectsForLeaderSkill.ts
@@ -1,10 +1,10 @@
-import { ILeaderSkill, PassiveEffect } from './datamine-types';
+import { ILeaderSkill, PassiveEffect } from '../datamine-types';
 
 /**
  * @description Get the effects of a given leader skill
  * @param skill leader skill to get the effects of
  * @returns the effects of the given leader skill if they exist, an empty array otherwise
  */
-export function getEffectsForLeaderSkill (skill: ILeaderSkill): PassiveEffect[] {
+export default function getEffectsForLeaderSkill (skill: ILeaderSkill): PassiveEffect[] {
 	return skill && Array.isArray(skill.effects) ? skill.effects : [];
 }
