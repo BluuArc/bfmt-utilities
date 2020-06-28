5993ee0cfe12ac4f8e11d820d99d2ec9f6870010
BluuArc <joshua.castor@joshuacastor.me>
2020-06-27 21:46:49 -0500

    Remove dev command for being too buggy and replace it with dev:single-run

--- tasks/index.js
+++ tasks/index.js
@@ -1,29 +1,13 @@
-const { watch, series } = require('gulp');
+const { series } = require('gulp');
 const buildTasks = require('./build');
 const testTasks = require('./test');
 const cleanupTasks = require('./cleanup');
-const utils = require('./utils');
 
 const fullBuild = series(testTasks.lintApplication, buildTasks.build, buildTasks.buildDocs);
-
-function runDev () {
-	const logger = utils.createLogger('Watcher');
-	function notifyReadiness (done) {
-		logger.log('watching for changes');
-		done();
-	}
-	const onChange = series(cleanupTasks.cleanupCoverageFiles, testTasks.runCoverage, fullBuild, notifyReadiness);
-	watch(['../src/**/*.ts', '../.eslintrc.ts.js', '!../src/**/*.d.ts', '!../src/version.ts'], onChange);
-	watch(['../typedoc.config.js', '../README.md'], series(buildTasks.buildDocs, notifyReadiness));
-	watch(['../src/**/*.spec.js', '../src/_test-helpers/**/*.js', '../jasmine.json', '../nyc.config.js'], series(testTasks.lintTests, cleanupTasks.cleanupCoverageFiles, testTasks.runCoverage, notifyReadiness));
-	watch(['./**/*.js'], series(testTasks.lintTasks, notifyReadiness));
-	return onChange();
-}
 
 module.exports = {
 	'build': fullBuild,
 	'build:dirty': buildTasks.build,
 	'update:data': buildTasks.updateBuffMetadata,
-	'dev': runDev,
 	'cleanup:coverage': cleanupTasks.cleanupCoverageFiles,
 };
