811d8e50f1dcdd8e11baa0b0be409d671bf89314
BluuArc <joshua.castor@joshuacastor.me>
2020-06-28 10:39:46 -0500

    Refactor how getNameForPassive is tested for clarity and sensibility

--- src/buffs/getNameForPassive.spec.js
+++ src/buffs/getNameForPassive.spec.js
@@ -3,6 +3,20 @@ const getNameForPassive = require('./getNameForPassive').default;
 const { PASSIVE_METADATA } = require('./buff-metadata');
 
 describe('getNameForPassive method', () => {
+	const expectEmptyString = (result) => {
+		expect(result)
+			.withContext('result is not an empty string')
+			.toBe('');
+	};
+
+	const expectNonEmptyString = (result, expected) => {
+		// use 2 expects to guard against result or expected being an empty string
+		expect(result.length)
+			.withContext('result is an empty etring')
+			.toBeGreaterThan(0);
+		expect(result).toBe(expected);
+	};
+
 	describe('for invalid metadata inputs', () => {
 		[
 			{
@@ -16,69 +30,50 @@ describe('getNameForPassive method', () => {
 		].forEach(testCase => {
 			it(`returns empty string if the metadata parameter ${testCase.name}`, () => {
 				const result = getNameForPassive(testConstants.KNOWN_ARBITRARY_PASSIVE_ID, testCase.value);
-
-				expect(typeof result === 'string')
-					.withContext('result is not a string')
-					.toBe(true);
-				expect(result.length)
-					.withContext('result is not an empty string')
-					.toBe(0);
+				expectEmptyString(result);
 			});
 		});
 	});
 
 	describe('when a valid metadata object is passed in', () => {
 		const arbitraryId = 'arbitrary id';
-		const arbitraryName = 'arbitrary name';
-		const arbitraryInvalidId = 'arbitrary invalid id';
+		const arbitraryMissingId = 'arbitrary missing id';
 
-		const generateMetadata = () => ({ [arbitraryId]: { ID: arbitraryId, Name: arbitraryName } });
 		[
-			{
-				input: arbitraryId,
-				name: 'a valid passive id',
-				shouldHaveEntry: true,
-			},
-			{
-				input: arbitraryInvalidId,
-				name: 'an invalid passive id',
-				shouldHaveEntry: false,
-			},
 			{
 				input: void 0,
 				name: 'an undefined id',
-				shouldHaveEntry: false,
 			},
 			{
 				input: null,
 				name: 'a null id',
-				shouldHaveEntry: false,
+			},
+			{
+				input: arbitraryMissingId,
+				name: 'a passive id not in the metadata',
+			},
+			{
+				input: arbitraryId,
+				name: 'a passive id without a Name',
 			},
 		].forEach(testCase => {
-			it(`returns ${testCase.shouldHaveEntry ? 'an object' : 'undefined'} for ${testCase.name}`, () => {
-				const metadata = generateMetadata();
+			it(`returns an empty string for ${testCase.name}`, () => {
+				const metadata = { [arbitraryId]: { ID: arbitraryId } };
 				const result = getNameForPassive(testCase.input, metadata);
-				expect(typeof result === 'string')
-					.withContext('result is not a string')
-					.toBe(true);
-				if (testCase.shouldHaveEntry) {
-					expect(result).toBe(arbitraryName);
-				} else {
-					expect(result.length)
-						.withContext('result is not an empty string')
-						.toBe(0);
-				}
+				expectEmptyString(result);
 			});
+		});
+
+		it('returns the Name field is of a given metadata entry', () => {
+			const arbitraryName = 'arbitrary name';
+			const metadata = { [arbitraryId]: { ID: arbitraryId, Name: arbitraryName } };
+			const result = getNameForPassive(arbitraryId, metadata);
+			expectNonEmptyString(result, arbitraryName);
 		});
 	});
 
 	it('defaults to PASSIVE_METADATA for metadata is not specified', () => {
 		const result = getNameForPassive(testConstants.KNOWN_ARBITRARY_PASSIVE_ID);
-		expect(result.length)
-			.withContext('result is an empty etring')
-			.toBeGreaterThan(0);
-		expect(result)
-			.withContext('result does not match metadata')
-			.toBe(PASSIVE_METADATA[testConstants.KNOWN_ARBITRARY_PASSIVE_ID].Name);
+		expectNonEmptyString(result, PASSIVE_METADATA[testConstants.KNOWN_ARBITRARY_PASSIVE_ID].Name);
 	});
 });
