28cee1397a0a4078e7572ae4836350933bd06cd5
BluuArc <joshua.castor@joshuacastor.me>
2020-07-03 19:49:36 -0500

    Add unit tests for buff-metadata

---
+++ src/buffs/parsers/buff-metadata.spec.js
@@ -0,0 +1,69 @@
+const { BUFF_METADATA } = require('./buff-metadata');
+const { BuffId, IconId } = require('./buff-types');
+const { getStringValueForLog } = require('../../_test-helpers/utils');
+
+
+describe('BUFF_METADATA entries', () => {
+	const expectIconsToBeValid = (icons = []) => {
+		icons.forEach((icon) => {
+			expect(icon in IconId)
+				.withContext(`"${icon}" should be in IconId enum`)
+				.toBeTrue();
+		});
+	};
+
+	const testDefaultIconResult = (buffId, expectedIcons) => {
+		it(`returns ${getStringValueForLog(expectedIcons)} for icons`, () => {
+			expect(BUFF_METADATA[buffId].icons()).toEqual(expectedIcons);
+			expectIconsToBeValid(expectedIcons);
+		});
+	};
+
+	const testIconResultWithBuff = (buffId, expectedIcons, buff, caseName) => {
+		it(`returns ${getStringValueForLog(expectedIcons)} for icons when ${caseName}`, () => {
+			expect(BUFF_METADATA[buffId].icons(buff)).toEqual(expectedIcons);
+			expectIconsToBeValid(expectedIcons);
+		});
+	};
+
+	it('all have functions for their "icons" property', () => {
+		Object.values(BUFF_METADATA).forEach((entry) => {
+			expect(typeof entry.icons)
+				.withContext(`${entry.id} should have function for icons property`)
+				.toBe('function');
+		});
+	});
+
+	describe('UNKNOWN_PASSIVE_EFFECT_ID', () => {
+		testDefaultIconResult(BuffId.UNKNOWN_PASSIVE_EFFECT_ID, [IconId.UNKNOWN]);
+	});
+
+	describe('passive:1:hp', () => {
+		testDefaultIconResult(BuffId['passive:1:hp'], [IconId.BUFF_HPUP]);
+		testIconResultWithBuff(BuffId['passive:1:hp'], [IconId.BUFF_HPDOWN], { value: -1 }, 'buff value is less than 0');
+	});
+
+	describe('passive:1:atk', () => {
+		testDefaultIconResult(BuffId['passive:1:atk'], [IconId.BUFF_ATKUP]);
+		testIconResultWithBuff(BuffId['passive:1:atk'], [IconId.BUFF_ATKDOWN], { value: -1 }, 'buff value is less than 0');
+	});
+
+	describe('passive:1:def', () => {
+		testDefaultIconResult(BuffId['passive:1:def'], [IconId.BUFF_DEFUP]);
+		testIconResultWithBuff(BuffId['passive:1:def'], [IconId.BUFF_DEFDOWN], { value: -1 }, 'buff value is less than 0');
+	});
+
+	describe('passive:1:rec', () => {
+		testDefaultIconResult(BuffId['passive:1:rec'], [IconId.BUFF_RECUP]);
+		testIconResultWithBuff(BuffId['passive:1:rec'], [IconId.BUFF_RECDOWN], { value: -1 }, 'buff value is less than 0');
+	});
+
+	describe('passive:1:crit', () => {
+		testDefaultIconResult(BuffId['passive:1:crit'], [IconId.BUFF_CRTRATEUP]);
+		testIconResultWithBuff(BuffId['passive:1:crit'], [IconId.BUFF_CRTRATEDOWN], { value: -1 }, 'buff value is less than 0');
+	});
+
+	describe('UNKNOWN_PROC_EFFECT_ID', () => {
+		testDefaultIconResult(BuffId.UNKNOWN_PROC_EFFECT_ID, [IconId.UNKNOWN]);
+	});
+});
